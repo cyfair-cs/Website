d8f5fe5a16776fc1860e4da7fc1bc35e33312eb3
Mufaro Machaya <sandmanmufmaf@gmail.com>
2022-07-22 21:23:26 -0500

    more express boilerplate

--- main.ts
+++ main.ts
@@ -6,6 +6,10 @@ const PORT = 3000;
 
 website.use(Express.static(Path.join(__dirname, 'public/')));
 
+website.listen(PORT, () => {
+    console.log('Started on port ' + PORT);
+});
+
 website.get('/', (req: Request, res: Response) => {
-    res.sendFile('index.html')
+    res.sendFile('index.html');
 });
