f2c833224d1b5494481654f0e995ad39f0190b52
Mufaro Machaya <sandmanmufmaf@gmail.com>
2022-08-15 23:13:52 -0500

    slight announcement loading changes

--- public/scripts/announcements.js
+++ public/scripts/announcements.js
@@ -40,7 +40,7 @@ class Announcement {
         ((this.imagelink == undefined || this.imagelink == null) ? '' :
         `       <img src=\"${this.imagelink}\" alt=\"${this.image_alt_text}\"> <br>`) +
 
-        `       <span class=\"announcement-text\">${this.text}</span>` + 
+        `       <span class=\"announcement-text\">${this.text}</span>` +
 
         `   </div>` +
         `</div>`;
@@ -55,13 +55,10 @@ class Announcement {
  */
  function load_announcements() {
     const ancments_db = [
-        new Announcement("Hello,", "World!"),
-        new Announcement("This is", "an announcement!"),
-        new Announcement("I love", "to code!"),
-        new Announcement("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "res/img/logo.png", "CFCSC LOGO"),
-        new Announcement("GO CYFAIR!", "This should be an image of the CyFair Computer Science Club\'s logo", "res/img/logo.png", "CFCSC LOGO")
+        new Announcement("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "../res/img/logo.png", "CFCSC LOGO"),
+        new Announcement('2021 District Competition', 'Depicted in the photo, you can see our 2021 district runner-up UIL Computer Science team! On the far left is our former president, Gavin Joyce, who placed 5th Individually, in the middle you can see our current president, Carson Stark, and on the right is the VP, Steven Nguyen, who placed 4th individually.', '../res/img/2021comp.png', '2021 District Competition')
     ];
-    
+
     const ancmnts_block = document.getElementById('announcements');
 
     for (let i = 0; i < ancments_db.length; i++)
