d79c8c2cfb0b4a59cbc5c49f98469fd738eaa043
Mufaro Machaya <sandmanmufmaf@gmail.com>
2022-08-16 00:27:29 -0500

    final changes before starting next full addition

--- public/scripts/announcements.js
+++ public/scripts/announcements.js
@@ -45,7 +45,6 @@ class Announcement {
  */
  function load_announcements() {
     const ancments_db = [
-        new Announcement("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", "../res/img/announcement/logo.png", "CFCSC LOGO"),
         new Announcement('2021 District Competition', 'Depicted in the photo, you can see our 2021 district runner-up UIL Computer Science team! On the far left is our former president, Gavin Joyce, who placed 5th Individually, in the middle you can see our current president, Carson Stark, and on the right is the VP, Steven Nguyen, who placed 4th individually.', '../res/img/announcement/2021comp.png', '2021 District Competition')
     ];
 
--- public/scripts/competitions.js
+++ public/scripts/competitions.js
@@ -18,13 +18,12 @@ class CompInfo {
         `<div class=\"compinfo card\" id=\"compinfo${index}\">` +
         `   <div class=\"title-date-row\">` +
         `       <span class=\"title\">${this.name}</span>` +
-        // , { weekday:"numeric", year:"numeric", month:"full", day:"numeric" })
         `       <span class=\"date\">${this.date.toLocaleDateString('en-us', { year:"numeric", month:"long", day:"numeric" })}</span>` +
         `   </div>` +
         `   <div class=\"button-row\">` +
         `       <button class=\"comp-signup\" onclick=\"window.location.href=\'${this.signupLink}\'\">Signup</button>` +
         (this.info_document_link === undefined ? '' :
-        `       <button class=\"comp-info-doc\" onclick=\"window.location.href=\'${this.info_document_link}\'\">Contest Information PDF</button>`) +
+        `       <button class=\"comp-info-doc\" onclick=\"window.location.href=\'${this.info_document_link}\'\">Contest Information</button>`) +
         `   </div>` +
         (this.description === undefined ? '' :
         `   <span class=\"description\">${this.description}</span>`) +
@@ -38,11 +37,10 @@ class CompInfo {
 function load_competitions() {
     const compinfo_db = [
         new CompInfo("Seven Lakes", "https://forms.gle/tdySEK1iR2ts5bkt6", "Our first competition of the year will be the annual Seven Lakes High School competition. Please sign up if you can go. Even if you are not experienced in doing any competition, this will be good practice.", "https://drive.google.com/file/d/119h1m2u6uWEwf_JtPq6b_-flqciFxVZv/view?usp=sharing", "2011-10-11"),
-        new CompInfo("Taylor HS", "https://forms.gle/tdySEK1iR2ts5bkt6", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.", undefined, '2001-10-12')
+        new CompInfo("Taylor HS", "https://forms.gle/tdySEK1iR2ts5bkt6", undefined, undefined, '2001-10-12')
     ]
 
     const compinfo_block = document.getElementById('competitions');
-    for (let i = 0; i < 3; i++)
     compinfo_db.forEach((compinfo, index) => {
         compinfo_block.innerHTML += compinfo.getHTMLComponent(index);
     })
